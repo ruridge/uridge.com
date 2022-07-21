bfc917c7be865565bcbb1f578c77005a2e71757c
Robert Uridge <856071+ruridge@users.noreply.github.com>
2022-07-21 23:07:46 +0800

    Bump favicon versions to clear cache

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,21 +11,22 @@ export const Layout = (props: LayoutProps) => {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/apple-touch-icon.png?v=1"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/favicon-32x32.png?v=1"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/favicon-16x16.png?v=1"
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest?v=1" />
+        <link rel="shortcut icon" href="/favicon.ico?v=1" />
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
