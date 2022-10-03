eff08e426fae433adccb6b466a18f586852149a0
Robert Uridge <856071+ruridge@users.noreply.github.com>
2022-10-03 21:52:16 +0800

    Init Astro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
