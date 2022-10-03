4e4e387dad8b611e123ce1ba8d102db472b16e74
Robert Uridge <856071+ruridge@users.noreply.github.com>
2022-10-03 21:52:16 +0800

    Init Astro

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+
+import solidJs from "@astrojs/solid-js";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [solidJs(), tailwind(), image(), sitemap()],
+});
