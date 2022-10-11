671e4081735ac445c7156d029356665ad9d82b6c
Robert Uridge <856071+ruridge@users.noreply.github.com>
2022-10-11 19:17:27 +0800

    Refactor to Tailwind CSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        "out-quint": "cubic-bezier(0.22, 1, 0.36, 1)",
+      },
+      transitionDuration: {
+        600: "600ms",
+      },
+      gridAutoColumns: {
+        "24ch": "24ch",
+      },
+      colors: {
+        solarized: {
+          700: "#083642",
+          800: "#032b36",
+          900: "#02212b",
+        },
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
