8e762746920e0d8869b58db9504d6cc6ad0ffb08
Robert Uridge <856071+ruridge@users.noreply.github.com>
2025-04-20 18:34:13 +0800

    Switch to @tailwindcss/vite plugin
    The commit removes the Astro Tailwind integration and replaces it with
    the native Vite plugin for Tailwind, while updating various
    configuration files and dependencies.
    
    Fixed layout overflow bug on blog post hero image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
@@ -7,19 +6,22 @@ import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { autolinkConfig } from "./plugins/rehype-autolink-config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://www.uridge.com/",
+
   integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
     sitemap(),
     mdx(),
     preact(),
   ],
+
   markdown: {
     rehypePlugins: [rehypeHeadingIds, [rehypeAutolinkHeadings, autolinkConfig]],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
